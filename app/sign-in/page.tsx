50b6a2b2c9a0f8ba070712bfe9a67f0732368d52
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-10-30 16:30:37 +0300

    design form  the  sign in

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { SigninBtns } from "@/components/SigninBtns"
+
+
+const SignIn = () => {
+  return (
+    <div className="">
+      <SigninBtns />
+    </div>
+  )
+}
+
+export default SignIn
---
+++ components/SigninBtns.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+export const SigninBtns = () => {
+  return (
+    <>
+    <h1 className='text-center mt-8'>Sign in</h1>
+    <div className='mt-4 p-4 flex flex-col items-center justify-center gap-4'>
+        <button className='flex items-center border p-4 rounded-full gap-4 hover:bg-slate-100/25 transition'>
+            <span>
+                <Image src={"/github-logo.png"}
+                width={30}
+                height={30}
+                alt='GitHub Logo'
+                 />
+            </span>
+            Sign In with Github
+        </button>
+
+        <button className='flex items-center border p-4 rounded-full gap-4 hover:bg-slate-100/25 transition'>
+            <span>
+                <Image src={"/google.logo.png"}
+                width={30}
+                height={30}
+                alt='GitHub Logo'
+                 />
+            </span>
+            Sign In with Google
+        </button>
+    </div>
+    </>
+  )
+}
